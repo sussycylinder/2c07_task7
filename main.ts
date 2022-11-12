cbe26de9c9a7da49f15e1f8e424d763da2ef6821
sussycylinder <115450379+sussycylinder@users.noreply.github.com>
2022-11-12 23:21:16 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.lightLevel())
+    led.setBrightness(255)
 })
+input.onButtonPressed(Button.B, function () {
+    led.setBrightness(0)
+})
+if (led.brightness() < input.lightLevel()) {
+    led.setBrightness(117)
+    basic.showNumber(led.brightness())
+} else {
+    led.setBrightness(132)
+    basic.showNumber(led.brightness())
+}
