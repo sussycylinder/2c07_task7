12777e58f1bfabc45210a38b5d3e926e8c38c091
sussycylinder <115450379+sussycylinder@users.noreply.github.com>
2022-11-14 22:29:30 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,23 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.lightLevel())
-})
-if (input.lightLevel() > led.brightness()) {
-    led.setBrightness(255)
-} else if (false) {
-	
-} else {
-	
+let mode = 0
+while (true) {
+    if (mode == 1) {
+        led.setBrightness(255 - input.lightLevel())
+    } else if (mode == 0) {
+        led.plotBarGraph(
+        input.lightLevel(),
+        255
+        )
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(input.lightLevel())
+        mode = 0
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        led.setBrightness(255 - input.lightLevel())
+        led.plotBarGraph(
+        255,
+        255
+        )
+        mode = 1
+    }
 }
